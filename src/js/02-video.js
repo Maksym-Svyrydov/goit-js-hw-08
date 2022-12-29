aa64f1ac1877b1959f622f6ef42d8a47a7a50312
Maksym-Svyrydov <110718671+Maksym-Svyrydov@users.noreply.github.com>
2022-12-29 16:03:03 +0100

    v 1.3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,23 @@
-import Player from '@vimeo/player';
-console.log(Player);
+import vimeoPlayer from '@vimeo/player';
+var throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new vimeoPlayer(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  }, 1000)
+);
+const timeFrame = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(timeFrame || 0.1).catch(function (error) {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+    default:
+      break;
+  }
+});
+console.log(typeof timeFrame);
